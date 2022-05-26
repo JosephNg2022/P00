4470a1060cd6a38c1778700f77b120ae693939cb
JosephNgJunWei <21018947@myrp.edu.sg>
2022-05-26 14:46:31 +0800

    first commit during class

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -26,51 +26,54 @@ public class CalculatorTest {
 
 	@Test
 	public void testAdd() {
+		// Arrange
 		int a = 1234;
 		int b = 8765;
-		
 		Calculator cal = new Calculator();
+		// Act
 		int actual = cal.add(a, b);
-		
+		// Assert
 		int expected = 9999;
 		assertEquals(expected, actual);
 	}
 	
 	@Test
 	public void testSubtract() {
+		// Arrange
 		int a = 9876;
 		int b = 4321;
-		
 		Calculator cal = new Calculator();
+		// Act
 		int actual = cal.subtract(a, b);
-		
+		// Assert
 		int expected = 5555;
 		assertEquals(actual, expected);
 	}
 	
 	@Test
 	public void testMultiple() {
+		// Arrange
 		int a = 5;
 		int b = 10;
-		
 		Calculator cal = new Calculator();
+		// Act
 		int actual = cal.multiple(a, b);
-		
+		// Assert
 		int expected = 50;
 		assertEquals(actual, expected);
 	}
 	
 	@Test
 	public void testDivide() {
+		// Arrange
 		double a = 5;
 		double b = 10;
-		
 		Calculator cal = new Calculator();
+		// Act
 		double actual = cal.divide(b, a);
-		
+		// Assert
 		double expected = 2;
-		int g = 1;
-		assertEquals(actual, expected, g);
-		System.out.println(actual);
+		int error = 2;
+		assertEquals(actual, expected, error);
 	}
 }
